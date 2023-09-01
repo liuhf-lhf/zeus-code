da15d841d8af1501da89b0f9229b8281f902558f
GitHub Action <action@github.com>
2023-09-01 06:56:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/liuhflhf/zeuscode/MyBundle.kt
+++ src/main/kotlin/com/github/liuhflhf/zeuscode/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.liuhflhf.zeuscode
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/liuhflhf/zeuscode/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/liuhflhf/zeuscode/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.liuhflhf.zeuscode.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.liuhflhf.zeuscode.MyBundle
+import com.github.liuhflhf.zeuscode.services.MyProjectService
 import javax.swing.JButton
 
 
